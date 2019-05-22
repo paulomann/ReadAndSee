Fixed the mean calculation for ELMo and FastText vectors.
Still not working for GPU training

readorsee/models/models.py
@@ -3,8 +3,6 @@ import torch.nn as nn
 import torch
 from allennlp.modules.elmo import Elmo, batch_to_ids
 from readorsee.data import config
-from gensim.models.fasttext import load_facebook_model
-
 
 class ResNet(nn.Module):
 
@@ -53,56 +51,43 @@ class ELMo(nn.Module):
         x = [sentence.split(" ") for sentence in x]
         print("\tIn Model: input size", len(x))
         x = batch_to_ids(x)
-        x = self.embedding(x)["elmo_representations"][0]
+        x = self.embedding(x)
+        mask = x["mask"]
+        x = x["elmo_representations"][0]
+        print(x)
         # This is where we get the mean of the word embeddings
-        x = torch.mean(x, dim=1)
+        x = self._get_mean(x, mask)
+        print(x)
+        # ----------------------------------------------------
         x = self.fc(x)
+        return x
+    
+    def _get_mean(self, x, mask):
+        x = x.sum(dim=1)
+        mask = mask.sum(dim=1).float()
+        mask = torch.repeat_interleave(mask, x.size(-1)).view(-1, x.size(-1))
+        x = torch.div(x, mask)
         return x
 
 class FastText(nn.Module):
     
-    def __init__(self, n_classes=2):
-        fasttext = self.load_fasttext_model()
-
+    def __init__(self, fasttext, n_classes=2):
+        super(FastText, self).__init__()
+        self.num_ftrs = fasttext.vector_size
         self.embedding = fasttext.wv
         self.fc = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(fasttext.vector_size, n_classes)
+            nn.Linear(self.num_ftrs, n_classes)
         )
 
     def forward(self, x):
         x = [sentence.split(" ") for sentence in x]
-        for sentence in x:
-            embedding = torch.mean([self.embedding[tok] for tok in sentence])
-            # TODO: append the embeddings mean altogether
-
-    def load_fasttext_model(self):
-        fasttext = load_facebook_model(
-            config.PATH_TO_FASTTEXT_PT_EMBEDDINGS, encoding="utf-8")
-        return fasttext
-        
-
-# elmo = ELMo()
-# gg = elmo(["eu gosto", "de deep learning"])
-
-FastText()
-
-
-
-
-    # def load_vectors(self):
-    #     fname = config.PATH_TO_FASTTEXT_PT_EMBEDDINGS
-    #     fin = io.open(fname, "r", encoding="utf-8", 
-    #                   newline="\n", errors="ignore")
-    #     vocab_size, embed_dimension = map(int, fin.readline().split())
-    #     data = {}
-    #     embeddings = torch.zeros((vocab_size, embed_dimension))
-    #     vocab = Counter()
-    #     for i, line in enumerate(fin):
-    #         tokens = line.rstrip().split(' ')
-    #         embedding = torch.tensor(list(map(float, tokens[1:])))
-    #         word = tokens[0]
-    #         data[word] = embedding
-    #         embeddings[i] = embedding
-    #         vocab[word] = i
-    #     return data
+        out = torch.zeros((len(x), self.num_ftrs))
+        for i, sentence in enumerate(x):
+            out[i] = self._get_mean(sentence)
+        x = self.fc(out)
+        return x
+    
+    def _get_mean(self, sentence):
+        tokens = torch.tensor([self.embedding[tok] for tok in sentence])
+        return torch.mean(tokens, dim=0)

readorsee/models/training.py
@@ -6,7 +6,8 @@ from torch.utils.data import DataLoader
 import copy
 import time
 from readorsee.data.dataset import DepressionCorpus
-from readorsee.models.models import ELMo, ResNet
+from readorsee.models.models import ELMo, ResNet, FastText
+from gensim.models.fasttext import load_facebook_model
 from readorsee.data import config
 import json
 
@@ -121,8 +122,12 @@ class Experiment():
 
         self.epochs = int(hyperparameters["general"]["num_epochs"])
         self.hyperparameters = hyperparameters
+
+        print("Loading FastText pretrained vectors...")
+        fasttext = self._load_fasttext_model()
+
         self.type_to_model = {"img": [ResNet(18), ResNet(34), ResNet(50)],
-                              "txt": [ELMo()],
+                              "txt": [ELMo(), FastText(fasttext)],
                               "both": []}
 
         self.experiment_type = experiment_type
@@ -131,6 +136,11 @@ class Experiment():
         if not isinstance(observation_periods, list):
             raise ValueError
         self.observation_periods = observation_periods
+
+    def _load_fasttext_model(self):
+        fasttext = load_facebook_model(
+            config.PATH_TO_FASTTEXT_PT_EMBEDDINGS, encoding="utf-8")
+        return fasttext
 
     def _get_dataloaders(self, days, dset):
         hparameters = self.hyperparameters["general"]