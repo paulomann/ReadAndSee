0aaf61550ec1f41685525a2d1acea7142ab04a48
holandafelipeuff <felipeholanda@id.uff.br>
2020-05-29 01:53:18 -0300

    using os.sep in path variables and adding a new one to the twitter dataset

--- readorsee/settings.py
+++ readorsee/settings.py
@@ -1,38 +1,43 @@
 from os.path import dirname, join
+import os
 
 ROOT = dirname(dirname(__file__))
 
-PATH_TO_QUESTIONNAIRE = join(ROOT, "data/raw/questionnaire.csv")
-PATH_TO_INSTAGRAM_DATA = join(ROOT, "data/external/instagram")
-PATH_TO_INTERIM_DATA = join(ROOT, "data/interim")
-PATH_TO_PROCESSED_DATA = join(ROOT, "data/processed")
+PATH_TO_QUESTIONNAIRE = join(ROOT, "data" + os.sep + "raw" + os.sep + "questionnaire.csv")
+PATH_TO_INSTAGRAM_DATA = join(ROOT, "data" + os.sep + "external" + os.sep + "instagram")
+PATH_TO_INTERIM_DATA = join(ROOT, "data" + os.sep + "interim")
+PATH_TO_PROCESSED_DATA = join(ROOT, "data" + os.sep + "processed")
 ENV_VARIABLES = join(ROOT, "instagram_access.env")
-PATH_TO_CLFS_OPTIONS = join(ROOT, "models/options.json")
+PATH_TO_CLFS_OPTIONS = join(ROOT, "models" + os.sep + "options.json")
 PATH_TO_EXPERIMENTS_OPTIONS = join(ROOT, "models", "experiments_options")
 PATH_TO_EXPERIMENTS = join(ROOT, "models", "experiments")
 PATH_TO_PT_LIWC = join(ROOT, "data", "LIWC2007_Portugues_win.dic")
 
 # ELMO related variables
 PATH_TO_EXTERNAL_TWITTER_DATA = join(ROOT,
-    "data/external/twitter/tweets_without_hashtag.txt")
+    "data" + os.sep + "external" + os.sep + "twitter" + os.sep + "tweets_without_hashtag.txt")
 PATH_TO_PROCESSED_TO_TRAIN_TWEETS = join(ROOT,
-    "data/processed/tweets/train_tweets/")
+    "data" + os.sep + "processed" + os.sep + "tweets" + os.sep + "train_tweets" + os.sep + "")
 PATH_TO_PROCESSED_TO_HELDOUT_TWEETS = join(ROOT,
-    "data/processed/tweets/heldout_tweets/")
-PATH_TO_VOCABULARY_DATA = join(ROOT, "data/processed/tweets/vocabulary.txt")
+    "data" + os.sep + "processed" + os.sep + "tweets" + os.sep + "heldout_tweets" + os.sep + "")
+PATH_TO_VOCABULARY_DATA = join(ROOT, "data" + os.sep + "processed" + os.sep + "tweets" + os.sep + "vocabulary.txt")
+
+# New twitter paths
+PATH_TO_EXTERNAL_TWITTER_DATASET = join(ROOT,
+    "data" + os.sep + "external" + os.sep + "twitter" + os.sep + "twitter_dataset.tsv")
 
 # Normal elmo:
-PATH_TO_ELMO_WEIGHTS = join(ROOT, "models/elmo/elmo_pt_weights.hdf5")
-PATH_TO_ELMO_OPTIONS = join(ROOT, "models/elmo/elmo_pt_options.json")
+PATH_TO_ELMO_WEIGHTS = join(ROOT, "models" + os.sep + "elmo" + os.sep + "elmo_pt_weights.hdf5")
+PATH_TO_ELMO_OPTIONS = join(ROOT, "models" + os.sep + "elmo" + os.sep + "elmo_pt_options.json")
     
 # Fine tuned elmo:
 PATH_TO_FT_ELMO_WEIGHTS = join(ROOT,
-    "models/elmo/fine_tuned_elmo/elmo_pt_fine_tuned_weights.hdf5")
+    "models" + os.sep + "elmo" + os.sep + "fine_tuned_elmo" + os.sep + "elmo_pt_fine_tuned_weights.hdf5")
 PATH_TO_FT_ELMO_OPTIONS = join(ROOT, 
-    "models/elmo/fine_tuned_elmo/elmo_pt_fine_tuned_options.json")
+    "models" + os.sep + "elmo" + os.sep + "fine_tuned_elmo" + os.sep + "elmo_pt_fine_tuned_options.json")
 
 # FastText related variables
-PATH_TO_FASTTEXT_PT_EMBEDDINGS = join(ROOT, "models/fasttext/cc.pt.300.bin.gz")
+PATH_TO_FASTTEXT_PT_EMBEDDINGS = join(ROOT, "models" + os.sep + "fasttext" + os.sep + "cc.pt.300.bin.gz")
 
 # BoW variables
 PATH_TO_SERIALIZED_TFIDF = join(ROOT, "models", "bow", "bow.model")
